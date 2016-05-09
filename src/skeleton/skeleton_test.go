b8484a831dd9b6ede6ef7c23d6841b55155b7b70
Christopher John Jackson <hello@cj-jackson.com>
2016-05-09 17:49:23 +0100

    Tested skeleton

---
+++ src/skeleton/skeleton_test.go
@@ -0,0 +1,27 @@
+package skeleton
+
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+	"strings"
+	"bytes"
+)
+
+func TestSkeleton(t *testing.T) {
+	Convey("Test output", t, func() {
+		testSubject := GetFormDemoSkeleton()
+
+		testSubject.SetTitle("This is the title")
+		testSubject.SetBody(strings.NewReader("<p>This is the body</p>"))
+
+		buf := &bytes.Buffer{}
+
+		testSubject.execute(buf)
+
+		str := buf.String()
+		buf.Reset()
+
+		So(strings.Index(str, "<title>This is the title"), ShouldNotEqual, -1)
+		So(strings.Index(str, "<p>This is the body</p>"), ShouldNotEqual, -1)
+	})
+}
