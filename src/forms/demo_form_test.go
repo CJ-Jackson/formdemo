ca1f79343ab5b35dcb785c37d77e4fea4c3236d0
Christopher John Jackson <hello@cj-jackson.com>
2016-05-10 19:53:20 +0100

    Tested form output

---
+++ src/forms/demo_form_test.go
@@ -0,0 +1,27 @@
+package forms
+
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+	"strings"
+)
+
+func TestDemoForm(t *testing.T) {
+
+	form := NewDemoForm()
+
+	form.TitleNorm = "Mr"
+	form.TimeZoneNorm = "Europe/Jersey"
+
+	Convey("Check output", t, func() {
+
+		str := string(form.HTML())
+
+		So(strings.Index(str, `value="Mr" selected>`), ShouldNotEqual, -1)
+		So(strings.Index(str, `value="Mrs">`), ShouldNotEqual, -1)
+
+		So(strings.Index(str, `value="Europe/Jersey" selected>`), ShouldNotEqual, -1)
+		So(strings.Index(str, `value="Europe/Paris">`), ShouldNotEqual, -1)
+
+	})
+}
